82a8d4a3128c2af281660ae245cf21e20af315bf
John Crouch <johntscrouch@gmail.com>
2025-12-01 13:48:43 -0600

    Switch DB to use DATABASE_URL for Railway

--- utils/db.js
+++ utils/db.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: 5432,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
